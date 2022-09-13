241809a685f364bb42afdb8e281dd23eeeba7c5c
GihanJeewantha <106438829+GihanJeewantha@users.noreply.github.com>
2022-09-13 23:20:45 +0530

    Update Main.py

--- Main.py
+++ Main.py
@@ -1,3 +1,15 @@
 print('I LOve Learn Codding')
 print("It's really good !!")
 print("My name is Jeewantha Dilshan")
+
+# variable
+
+name = 'Jeewantha '
+last_name = 'Dilshan'
+print(name)
+print("Hello"+name)
+print(type(name))
+full_name = name + last_name
+print(full_name)
+
+
