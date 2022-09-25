3a974d670d993a6f89aa53753361ac8f99678a40
GihanJeewantha <106438829+GihanJeewantha@users.noreply.github.com>
2022-09-25 22:10:51 +0530

    Create app.py

---
+++ app.py
@@ -0,0 +1,17 @@
+print('hello world')
+
+#variable
+
+age = 23
+age = 30
+print(age)
+
+price = 19.95
+first_name = "jeewantha"
+last_name = "Dilshan"
+
+print(first_name +" "+ last_name)
+print(price)
+
+name=input("Enter you'r name: ")
+print("hello "+name)
