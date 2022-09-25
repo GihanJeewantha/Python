9aa14d72624217f13ce90c833290fb4ba80dc219
GihanJeewantha <106438829+GihanJeewantha@users.noreply.github.com>
2022-09-25 22:11:35 +0530

    If condition

---
+++ basic.py
@@ -0,0 +1,15 @@
+
+#conver types
+
+birth_year = input("Enther birthday: ")
+age = 2022 - int(birth_year)
+print("your age  is: "+str(age))
+
+# example 2
+
+first = input("num1")
+second = input("num2")
+
+sum = float(first) + float(second)
+print(sum)
+
---
+++ if.py
@@ -0,0 +1,11 @@
+temp = 30
+
+if(temp>30):
+    print("Hot")
+    print("drink plenty of water")
+
+elif temp >20:
+    print("its a nice day")
+
+else:
+    print("done")
