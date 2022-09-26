fcd49372ff9c6167e2363eba81d7ae9823672a46
GihanJeewantha <106438829+GihanJeewantha@users.noreply.github.com>
2022-09-26 23:36:05 +0530

    Create functions.py

---
+++ functions.py
@@ -0,0 +1,24 @@
+#building function
+import math
+import datetime
+
+x=4**2
+print(x)
+
+b=pow(4,2)
+print(b)
+
+a=help(pow)
+
+m=max(4,5,2,8,5)
+print(m)
+
+q=math.sqrt(100)
+print(q)
+
+date=datetime.datetime.now()
+print(date)
+
+
+
+
