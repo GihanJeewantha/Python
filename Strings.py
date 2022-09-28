f2fd8b677a711c132c84d51e30c173bfedbfe271
GihanJeewantha <106438829+GihanJeewantha@users.noreply.github.com>
2022-09-28 23:57:33 +0530

    Create Strings.py

---
+++ Strings.py
@@ -0,0 +1,22 @@
+x= 'Jeewantha dilshan'
+b = 'learning python'
+c= "hi i'm jeewantha Dilshan i love learn python"
+
+print(x,b,c)
+
+j= 'Hi this is ussing \' here '
+
+print(j)
+
+print(x.capitalize())
+print(x.upper())
+print(x.lower())
+
+print(x[3])
+print(x[2:6]) #ussing print range
+print(x[3:]) #print all after 3rd index
+print(x[4:]) #befor 4
+
+a= 'Gihan'
+print(a)
+print(a.replace('Gihan','JD'))
